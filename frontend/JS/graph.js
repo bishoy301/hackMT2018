d32047882de0547311a99adb320f5461ae6e88f0
Bishoy Boktor <bishoy.boktorsd@mtsu.edu>
2018-01-27 17:07:53 -0600

    Possible Happiness

--- frontend/JS/graph.js
+++ frontend/JS/graph.js
@@ -1,109 +1,70 @@
-//soooo much stack overflow / random other sites / done quickly - so any love is good love
-var url = "http://www.gw2spidy.com/api/v0.9/json/listings/19729/buy/1";
+  // set the dimensions and margins of the graph
+  var margin = {top: 20, right: 20, bottom: 30, left: 50},
+  width = 960 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
+
+// parse the date / time
+var parseTime = d3.timeParse("%d-%b-%y");
+
+// set the ranges
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
+
+// define the line
+var valueline = d3.line()
+  .x(function(d) { return x(new Date(d.listing_datetime)); })
+  .y(function(d) { return y(d.unit_price); });
+
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg = d3.select("body").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform",
+  "translate(" + margin.left + "," + margin.top + ")");
+
+// Get the data
+var url = "https://www.gw2spidy.com/api/v0.9/json/listings/19729/buy/1";
 var xhr = new XMLHttpRequest();
 xhr.open('GET', url, true);
+//xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
 xhr.send();
 
 xhr.addEventListener("readystatechange", processRequest, false);
 //helper - this could probably be more general
 
-function maxPrice(data){
-        var m = -Infinity, i = 0, n = data.results.length;
-        for (; i != n; ++i){
-            if (data.results[i].unit_price > m) {
-                m = data.results[i].unit_price;
-            }
-        }
-        return m;
-}
-
-function getDate(data) {
-    return new Date(data.results.listing_datetime);
-}
-
 function processRequest(e) {
 if (xhr.readyState == 4 && xhr.status == 200) {
-    var newString = xhr.responseText;
-    var data = JSON.parse(xhr.responseText);
-    //alert(response.results[0].unit_price);
-            /* implementation heavily influenced by http://bl.ocks.org/1166403 */
-            // NEEDS CHANGE FOR % SCREEN WIDTH
-            var m = [80, 80, 80, 80]; // margins
-            var w = 1000 - m[1] - m[3]; // width
-            var h = 400 - m[0] - m[2]; // height
+  var newString = xhr.responseText;
+  var data = JSON.parse(xhr.responseText);
 
 
-//another helper function
-// helper function
+  var results = data.results;
+  console.log("yo this is data:", results);
 
-//possible helpful code if date is formatted correctly ie not like the dummy JSON
-/*// helper function
-function getDate(d) {
-return new Date(d.jsonDate);
+
+
+  // Scale the range of the data
+  x.domain(d3.extent(results, function(d) { return new Date(d.listing_datetime); }));
+  y.domain([0, d3.max(results, function(d) { return d.unit_price; })]);
+
+  // Add the valueline path.
+  svg.append("path")
+          .data([results])
+          .attr("class", "line")
+          .attr("d", valueline);
+
+  // Add the X Axis
+  svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
+
+  // Add the Y Axis
+  svg.append("g")
+          .call(d3.axisLeft(y));
+
 }
 
-// get max and min dates - this assumes data is sorted
-var minDate = getDate(data[0]),
-maxDate = getDate(data[data.length-1]);
-
-var x = d3.time.scale().domain([minDate, maxDate]).range([0, w]);
-
-Then you don't need to deal with the time interval functions, you can just pass x a date:
-
-.attr("d", d3.svg.line()
-.x(function(d) { return x(getDate(d)) })
-.y(function(d) { return y(d.jsonHitCount) })
-);*/
-
-            // X scale will fit all values from data[] within pixels 0-w
-            var x = d3.scale.linear().domain([0, data.count]).range([0, w]);
-            // Y scale will fit values from 0-10 within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
-            var y = d3.scale.linear().domain([0, maxPrice(data)]).range([h, 0]);
-                // automatically determining max range can work something like this
-                // var y = d3.scale.linear().domain([0, d3.max(data)]).range([h, 0]);
-
-            // create a line function that can convert data[] into x and y points
-            var line = d3.svg.line()
-                // assign the X function to plot our line as we wish
-                .x(function(d,i) {
-                    // verbose logging to show what's actually being done
-                    console.log('Plotting X value for data point: ' + d + ' using index: ' + i + ' to be at: ' + x(i) + ' using our xScale.');
-                    // return the X coordinate where we want to plot this datapoint
-                    return x(i);
-                })
-                .y(function(d) {
-                    // verbose logging to show what's actually being done
-                    console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d.unit_price) + " using our yScale.");
-                    // return the Y coordinate where we want to plot this datapoint
-                    return y(d.unit_price);
-                })
-
-                // Add an SVG element with the desired dimensions and margin.
-                var graph = d3.select("#graph").append("svg:svg")
-                      .attr("width", w + m[1] + m[3])
-                      .attr("height", h + m[0] + m[2])
-                    .append("svg:g")
-                      .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
-
-                // create yAxis
-                var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
-                // Add the x-axis.
-                graph.append("svg:g")
-                      .attr("class", "x axis")
-                      .attr("transform", "translate(0," + h + ")")
-                      .call(xAxis)
-
-
-                // create left yAxis
-                var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
-                // Add the y-axis to the left
-                graph.append("svg:g")
-                      .attr("class", "y axis")
-                      .attr("transform", "translate(-25,0)")
-                      .call(yAxisLeft);
-
-                  // Add the line by appending an svg:path element with the data line we created above
-                // do this AFTER the axes above so that the line is above the tick-lines
-                  graph.append("svg:path").attr("d", line(data.results));
-}
-}
+};
