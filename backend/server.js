062f33be9ad19586d01951a259194c66edc68520
Madison Karrh <mtk2r@mtmail.mtsu.edu>
2018-01-27 14:52:31 -0600

    Backend without firebase save and old code just in case.

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,117 @@
-function runService() {
-    // Pull all current items from prices api
-    let items = [];
+const https = require('https');
+const admin = require("firebase-admin");
+const RateLimiter = require("request-rate-limiter");
 
-    // For each item, validate whether its worth checking and evaluate it
-    items.forEach((item) => {
-        evaluateItem();
-    })
+var serviceAccount = require("./hackmt2018-ebd27-firebase-adminsdk-d6psx-c9249b56da.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://hackmt2018-ebd27.firebaseio.com"
+});
+
+var db = admin.database();
+var ref = db.ref("restricted_access/secret_document");
+
+var limiter = new RateLimiter();
+
+async function runService() {
+    try {
+        // Pull all crafting material ids
+        // let craftingIds = await httpRequest({
+        //     host: 'api.guildwars2.com',
+        //     port: 443,
+        //     method: 'GET',
+        //     path: '/v2/materials'
+        // });
+
+        let response = await limiter.request({
+            url       : 'https://api.guildwars2.com/v2/materials',
+            method    : 'get'
+        });
+
+        let craftingIds = JSON.parse(response.body);
+
+        //console.log(craftingIds);
+
+        for (craftingId of craftingIds) {
+            // Pull all current items from prices api
+            // let craftingItemsList = await httpRequest({
+            //     host: 'api.guildwars2.com',
+            //     port: 443,
+            //     method: 'GET',
+            //     path: `/v2/materials/${craftingId}`
+            // });
+
+            response = await limiter.request({
+                url       : `https://api.guildwars2.com/v2/materials/${craftingId}`,
+                method    : 'get'
+            });
+            
+            let craftingItemsList = JSON.parse(response.body);
+
+            let itemIds = [];
+
+            if (craftingItemsList.items) {
+                itemIds = craftingItemsList.items;
+            }
+
+            // For each item, validate whether its worth checking and evaluate it
+            for (itemId of itemIds) {
+                 await evaluateItem(itemId);
+            }
+        }
+    } catch(err) {
+        console.error(err);
+    }
 
     restartService();
 }
 
-function evaluateItem() {
+async function evaluateItem(id) {
+    // let itemInfo = await httpRequest({
+    //     host: 'api.guildwars2.com',
+    //     port: 443,
+    //     method: 'GET',
+    //     path: `/v2/items/${id}`
+    // });
+
+    let response = await limiter.request({
+        url       : `https://api.guildwars2.com/v2/items/${id}`,
+        method    : 'get'
+    });
     
-}
+    let itemInfo = JSON.parse(response.body);
 
-function getPrices() {
+    let flags = itemInfo.flags;
+    let found = flags.find((element) => {
+        return (element === 'NoSell');
+    });
+    if (found) return;
 
+    // let itemPrices = await httpRequest({
+    //     host: 'api.guildwars2.com',
+    //     port: 443,
+    //     method: 'GET',
+    //     path: `/v2/commerce/prices/${id}`
+    // });
+
+    response = await limiter.request({
+        url       : `https://api.guildwars2.com/v2/commerce/prices/${id}`,
+        method    : 'get'
+    });
+
+    let itemPrices = JSON.parse(response.body);
+
+    //console.log(itemInfo);
+
+    if (itemPrices.buys.quantity > 10000 && itemPrices.sells.quantity > 10000) {
+        // save to firebase
+        console.log(itemPrices.buys.quantity);
+        console.log(itemPrices.sells.quantity);
+
+    } else {
+        return;
+    }
 }
 
 function saveToFirebase() {
@@ -24,6 +120,44 @@ function saveToFirebase() {
 
 function restartService() {
     // Set timeout for 15 minutes (?) and call runService
+    setTimeout(runService, 10000);
+    console.log('loopin');
 }
+
+// function httpRequest(params, postBody) {
+//     //let _params = params;
+//     return new Promise((resolve, reject) => {
+//         let req = https.request(params, (res) => {
+//             if (res.statusCode < 200 || res.statusCode >= 300) {
+//                 return reject(new Error(`Unexpected ${res.statusCode} hitting ${params.path}`));
+//             }
+//             let body = [];
+
+//             res.on('data', (chunk) => {
+//                 body.push(chunk);
+//             })
+
+//             res.on('end', () => {
+//                 try {
+//                     body = JSON.parse(Buffer.concat(body).toString());
+//                 } catch(err) {
+//                     reject(err);
+//                 }
+
+//                 resolve(body);
+//             });
+//         });
+
+//         req.on('error', (err) => {
+//             reject(err);
+//         });
+
+//         if (postBody) {
+//             req.write(postBody);
+//         }
+
+//         req.end();
+//     });
+// }
 
 runService();
